693b9b84294b6099ba90a56265aa45b5445e2804
alvaropaniagua <alvaro.paniagua@indra.int>
2019-05-31 15:12:21 +0200

    added script for copying csv files from sftp to magento dev

---
+++ sftp_magento_files.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+        ftp_site="10.20.1.112"
+        username="motortown_watcher"
+#source path
+        spath="./var/import"
+#Remote Path
+        rpath="/anjana/motortown_pro/ingesta_productos_stock/procesado"
+
+        cd $spath
+
+        fileName=$(echo "ls -1rt" | sftp $username@$ftp_site:$rpath | grep "magento_csv_products" | tail -1)
+	fileNameStock=$(echo "ls -1rt" | sftp $username@$ftp_site:$rpath | grep "magento_csv_stock" | tail -1)
+
+	echo "get $fileName" |sftp $username@$ftp_site:$rpath/$fileName /var/www/magentodev/var/import/products/current.zip
+	echo "get $fileNameStock" |sftp $username@$ftp_site:$rpath/$fileNameStock /var/www/magentodev/var/import/avilability/current.csv
+	
+#	echo $rpath/$fileName " and " $rpath/$fileNameStock "copied to" $spath
+
+	exit
+        EOF
+        echo "Success : $?"
