762a0d8aa09a3f7a0e28d923b8398268cc3e7eba
alvaropaniagua <alvaro.paniagua@indra.int>
2019-06-21 14:58:20 +0200

    fixed scripts

--- import_magento_files.sh
+++ import_magento_files.sh
@@ -9,30 +9,62 @@
 #Remote Path
         rpath="/anjana/motortown_pro/ingesta_productos_stock/procesado"
 
-        cd $spath
-
-        fileName=$(echo "ls -1rt" | sftp $username@$ftp_site:$rpath | grep "magento_csv_products" | tail -1) 
-	fileNameStock=$(echo "ls -1rt" | sftp $username@$ftp_site:$rpath | grep "magento_csv_stock" | tail -1)
-
-	echo -e "\nstart of log\n"
-	echo "get $fileName" | sftp $username@$ftp_site:$rpath/$fileName /var/www/magentodev/var/import/product/current.csv  
-	echo "get $fileNameStock" | sftp $username@$ftp_site:$rpath/$fileNameStock /var/www/magentodev/var/import/avilability/current.csv 
 	
-	echo -e "\nCurl custom product import:"  
-	curl $magentourl/Customcart/Import/product
-	sleep 180
-	#echo -e "\nOutput of system.log: "
-	#tail $magentopath/var/log/system.log | grep "STRATIO-products" 
+        fileName=$(echo "ls -1" | sftp $username@$ftp_site:$rpath | grep "magento_csv_products" | tail -1) 
+	fileNameStock=$(echo "ls -1" | sftp $username@$ftp_site:$rpath | grep "magento_csv_stock" | tail -1)
+	
+	filelist=$(echo "ls -1" | sftp $username@$ftp_site:$rpath | grep "magento_csv_products")	
+	filelistStock=$(echo "ls -1" | sftp $username@$ftp_site:$rpath | grep "magento_csv_stock")
 
-	echo -e "\nCurl custom stock import:"
-	curl $magentourl/Customcart/Import/availabilitysingle
-	sleep 180
+	declare -a arr=($filelist)
+	arraylength=${#arr[@]}
+
+	for (( i=0; i<${arraylength}; i++ ))
+	do
+	   # copiar solo si no esta en lista de leidos
+	   if grep -q ${arr[$i]} "$magentopath/var/import/ficheros_leidos.txt"; 
+	   then
+   	   	echo "${arr[$i]} already imported."
+	   else
+ 	        echo "${arr[$i]} is new and will be imported."
+		echo "get ${arr[$i]}" | sftp $username@$ftp_site:$rpath/${arr[$i]} $magentopath/var/import/product/current.csv  
+		curl $magentourl/Customcart/Import/product
+	        sleep 90 
+		
+		echo ${arr[$i]} >> $magentopath/var/import/ficheros_leidos.txt
+
+		break
+	   fi	
+	done
+	
+	echo " --- "
+
+
+	declare -a arrStock=($filelistStock)
+        arraylengthStock=${#arrStock[@]}
+
+        for (( j=0; j<${arraylengthStock}; j++ ))
+        do
+           # copiar solo si no esta en lista de leidos
+           if grep -q ${arrStock[$j]} "$magentopath/var/import/ficheros_leidos.txt"; 
+           then
+                echo "${arrStock[$j]} already imported."
+           else
+                echo "${arrStock[$j]} is new and will be imported."
+                echo ${arrStock[$j]} >> $magentopath/var/import/ficheros_leidos.txt
+                echo "get ${arrStock[$j]}" | sftp $username@$ftp_site:$rpath/${arrStock[$j]} $magentopath/var/import/avilability/current.csv  
+                curl $magentourl/Customcart/Import/availabilitysingle
+                sleep 90
+
+                break
+           fi   
+        done
+	
 	echo -e "\nOutput of system.log: "
-	tail -n 50 $magentopath/var/log/system.log | grep "STRATIO"
+	tail -n 20 $magentopath/var/log/system.log | grep "STRATIO"
 							
-	#$magentopath/php bin/magento indexer:reindex
+	echo "Success : $?"
 
 	exit
         EOF
         echo "Success : $?"
-
--- import_magento_files_job.sh
+++ import_magento_files_job.sh
@@ -2,9 +2,8 @@
 magentopath="/var/www/magentodev"
 hoy=`date +%Y_%m_%d_%H_%M`
 
-$magentopath/import_magento_files.sh > $magentopath/var/log/import_$hoy.log
+$magentopath/var/import/import_magento_files.sh > $magentopath/var/log/import_$hoy.log
 
-php bin/magento indexer:reindex >> $magentopath/var/log/import_$hoy.log
+#php bin/magento indexer:reindex >> $magentopath/var/log/import_$hoy.log
 
 echo -e "\nFinished job" >> $magentopath/var/log/import_$hoy.log
-
